21f2542cb58d22b62fc027cf9f53cbed9cdad4f7
abdulkadiraktas <abdulkadir.aktas@hotmail.com>
2019-06-14 10:56:14 +0300

    Create tr.lua
    added tr.lang

---
+++ locales/tr.lua
@@ -0,0 +1,6 @@
+Locales['tr'] = {
+  ['skin_menu'] = 'Karakter Tasarım Menüsü',
+  ['use_rotate_view'] = '~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ ve ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ tuşlarını kullanarak kamerayı döndürebilirsin.',
+  ['skin'] = 'Karakter tasarım',
+  ['saveskin'] = 'Karakter tasarımını dosyaya kaydet',
+}
